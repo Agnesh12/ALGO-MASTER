a69cc3f38cb6988c0d321158a7b089e0a1e1d1b5
AGNESH K <agnesh9025@gmail.com>
2025-03-04 19:42:22 +0530

    program

---
+++ src/Arrays/majorityelement.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class majorityelement {
+    static void major(int[] arr) {
+        int count  = 0;
+        int element = 0;
+         for(int i = 0; i < arr.length; i++) {
+             if(count == 0) {
+                 count = 1;
+                 element = arr[i];
+             }
+             else if(arr[i] == element) {
+                 count++;
+             }
+             else {
+                 count--;
+             }
+         }
+         int count1 = 0;
+         for(int i = 0; i < arr.length; i++) {
+             if(arr[i] == element) {
+                 count1++;
+             }
+         }
+         if(count1 > arr.length / 2) {
+             System.out.println(element);
+         }
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 1, 2, 2, 2};
+        major(arr);
+    }
+}
