d5decbfc14e259109dcf032009f631968f5c322f
AGNESH K <agnesh9025@gmail.com>
2025-03-04 19:48:09 +0530

    program

---
+++ src/Arrays/movezeros.java
@@ -0,0 +1,27 @@
+package Arrays;
+import java.util.Arrays;
+public class movezeros {
+    static void zeros(int[] arr) {
+        int left = 0;
+        int mid = 0;
+        int right = arr.length - 1;
+         while(mid <= right) {
+             if(arr[mid] != 0) {
+                 int temp = arr[mid];
+                 arr[mid] = arr[left];
+                 arr[left] = temp;
+                 left++;
+                 mid++;
+             }
+             else{
+                 mid++;
+             }
+
+         }
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 2, 0, 5, 6};
+        zeros(arr);
+    }
+}
