0c6620b8be4c53de2332621af0a6e9094aa666ad
AGNESH K <agnesh9025@gmail.com>
2025-03-05 10:23:23 +0530

    program

---
+++ src/Arrays/removeduplicates.java
@@ -0,0 +1,19 @@
+package Arrays;
+import java.util.Arrays;
+public class removeduplicates {
+    static void remove(int[] arr) {
+        int[] ans = new int[arr.length];
+        int temp = 0;
+        ans[temp] = arr[0];
+        for(int i = 0; i < arr.length - 1; i++) {
+            if(arr[i] != arr[i + 1]) {
+                ans[temp++] = arr[i + 1];
+            }
+        }
+        System.out.println(Arrays.toString(ans));
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 2, 3, 3, 4, 4};
+        remove(arr);
+    }
+}
