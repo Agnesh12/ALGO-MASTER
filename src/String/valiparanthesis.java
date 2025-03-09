3dff82b93849306fc277f8624d6f21491c0ebdb3
AGNESH K <agnesh9025@gmail.com>
2025-03-09 16:28:28 +0530

    program

---
+++ src/String/valiparanthesis.java
@@ -0,0 +1,23 @@
+package String;
+import java.util.Stack;
+public class valiparanthesis {
+    static boolean valid(String str) {
+        Stack<Character> ans = new Stack();
+        for(char ch : str.toCharArray()) {
+            if (ch == '(') {
+                ans.push(')');
+            } else if (ch == '[') {
+                ans.push(']');
+            } else if (ch == '{') {
+                ans.push('}');
+            } else if (ans.isEmpty() || ans.pop() != ch) {
+                return false;
+            }
+        }
+            return ans.isEmpty();
+    }
+    public static void main(String[] args) {
+        String str = "(){}[]";
+        System.out.print(valid(str));
+    }
+}
